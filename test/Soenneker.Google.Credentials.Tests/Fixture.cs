e9ef8aec1a0666ae70b2901433f06f903cf42954
Jake Soenneker <jake@soenneker.com>
2025-06-12 08:54:03 -0500

    hosting

--- test/Soenneker.Google.Credentials.Tests/Fixture.cs
+++ test/Soenneker.Google.Credentials.Tests/Fixture.cs
@@ -1,9 +1,11 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Serilog;
 using Soenneker.Fixtures.Unit;
-using Soenneker.Utils.Test;
 using Soenneker.Google.Credentials.Registrars;
+using Soenneker.Utils.Test;
 
 namespace Soenneker.Google.Credentials.Tests;
 
@@ -25,6 +27,8 @@ public class Fixture : UnitFixture
 
         IConfiguration config = TestUtil.BuildConfig();
         services.AddSingleton(config);
+
+        services.AddSingleton<IHostEnvironment, HostingEnvironment>();
 
         services.AddGoogleCredentialsUtilAsScoped();
     }
